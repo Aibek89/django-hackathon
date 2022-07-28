52f821449e284282144b402de9843161845f312a
Aibek8989 <aibekemil8989@gmail.com>
2022-07-28 20:17:56 +0600

    regstration done

--- account/views.py
+++ account/views.py
@@ -1,3 +1,21 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from account.serializers import RegisterSerializer
+
+
+class RegisterApiView(APIView):
+    def post(self, request):
+        print(dir(request))
+        data = request.data
+        serializer = RegisterSerializer(data=data)
+
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            message = f'Вы успешно зарегистрированы.' \
+                      f'Вам отправлено письмо с активацией'
+
+            return Response(message, status=201)
+        print('hey')
