7246eb1360fc00c4a20f6546d2340aa9b4809474
Aibek8989 <aibekemil8989@gmail.com>
2022-07-29 19:49:36 +0600

    change login

--- account/serializers.py
+++ account/serializers.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
 
 from account.send_mail import send_confirmation_email
@@ -29,11 +29,25 @@ class RegisterSerializer(serializers.ModelSerializer):
         return user
 
 
-class LoginSerializer(serializers.ModelSerializer):
+class LoginSerializer(serializers.Serializer):
     email = serializers.EmailField(required=True)
     password = serializers.CharField(required=True)
 
 
-    class Meta:
-        model = User
-        fields = ['email', 'password']
+    def validate_email(self, email):
+        if not User.objects.filter(email=email).exists():
+            raise serializers.ValidationError('Пользователь не зарегистрирован')
+        return email
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        if email and password:
+            user = authenticate(username=email, password=password)
+
+            if not user:
+                raise serializers.ValidationError('Неверный email или пароль')
+
+            attrs['user'] = user
+            return attrs
